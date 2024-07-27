1043c4007670f490fdc7e77679fa2b9e93edd5a2
DuLoS-M <e_craud@hotmail.com>
2024-07-27 08:53:44 -0600

    Initial

---
+++ src/app/admin/dish-list/dish-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dish-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './dish-list.component.html',
+  styleUrl: './dish-list.component.scss'
+})
+export class DishListComponent {
+
+}
