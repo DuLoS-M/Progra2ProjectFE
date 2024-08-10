d281912f47939802af5b9b7eb4d390f32189218b
DuLoS-M <e_craud@hotmail.com>
2024-08-10 09:31:11 -0600

    Created login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from "./admin/auth/login.component";
+import {PrimeNGConfig} from 'primeng/api';
+import { OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'ProjectFE';
+export class AppComponent implements OnInit {
+
+  constructor(private primengConfig: PrimeNGConfig) {}
+
+  ngOnInit() {
+      this.primengConfig.ripple = true;
+  }
 }
